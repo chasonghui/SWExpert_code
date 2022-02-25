7cf98b3c39c990bde50ae739606c0ab2dfe1c1a6
차송희 <chasonghui@naver.com>
2022-02-25 16:09:10 +0900

    농작물 수확하기

--- D3/SWEA_2805.java
+++ D3/SWEA_2805.java
@@ -1,48 +1,50 @@
-package m1week4;
+package Eight;
+
 import java.util.Scanner;
 
-/** SW Expert D3 2805 농작물 수확 **/
-public class SWEA_2805 {
+/*
+ * IM대비 2차
+ * 20220225
+ * D3 농작물 수확하기
+ */
+public class 농작물수확하기 {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
-
-		for (int test_case = 1; test_case <= T; test_case++) {
-			int sum = 0;
+		for (int tc = 1; tc <= T; tc++) {
 			int N = sc.nextInt();
-			int[][] farm = new int[N][N];
+			int[][] map = new int[N][N];
 
-//			for (int i = 0; i < N; i++) {
-//				for (int j = 0; j < N; j++) {
-//					farm[i][j] = sc.nextInt();
-//				}
-//			}
+			int sum = 0; // 농작물 수익
 
-			// 배열에 값 넣기
-			for (int i = 0; i < N; i++) {
+			// 맵 입력
+			for (int i = 0; i < map.length; i++) {
 				String str = sc.next();
-				for (int j = 0; j < str.length(); j++) {
-					farm[i][j] = str.charAt(j) - '0';
+				for (int j = 0; j < map.length; j++) {
+					map[i][j] = str.charAt(j) - '0';
 				}
 			}
 
-			// 마름모탐색-------------------------------------
-
-			// 마름모 윗부분
-			for (int i = 0; i < (N / 2); i++) {
-				for (int j = (N / 2) - i; j <= (N / 2) + i; j++) {
-					sum += farm[i][j];
+			// 마름모 탐색
+			int range = 0;
+			for (int i = 0; i < N; i++) {
+				for (int j = N / 2 - range; j <= N / 2 + range; j++) {
+					sum += map[i][j];
 				}
-			}
-			// 마름모 아랫부분
-			for (int i = (N / 2); i >= 0; i--) {
-				for (int j = (N / 2) - i; j <= (N / 2) + i; j++) {
-					sum += farm[N - i - 1][j];
+				// System.out.println("range: " + range);
+				// System.out.println(sum);
+				
+				// 아래 삼각형인 경우 range--
+				if (i >= N / 2) {
+					range--;
+				} else {// 위 삼각형인경우 range++;
+					range++;
 				}
+
 			}
 
-			System.out.println("#" + test_case + " " + sum);
+			System.out.println("#" + tc + " " + sum);
 		}
 
 	}
